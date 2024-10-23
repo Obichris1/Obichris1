914c0ea6a7940bec1dabc5aa8d0d5f74d6ebc7ec
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2024-10-23 15:37:11 +0100

    Fixes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
  <div id='about'>
    <div className='about-content'>
-        <p> Hi!! I'm Obinna,  I am a trained computer engineer with a specialization in frontend development. I have 3 years of experience working with frontend technologies and a year of professional experience.I am excited about creating responsive and intuitive interfaces that not only meet users' needs but also leave a lasting positive impression. Currently, I work as a frontend developer at Simplify Technologies remotely where I work closely with the backend team to create services for microfinance organizations.</p>
+        <p> Hi!! I'm Obinna, A trained computer engineer with a specialization in frontend development. I have 3 years of experience working with frontend technologies and a year of professional experience. I am excited about creating responsive and intuitive interfaces that not only meet users' needs but also leave a lasting positive impression. Currently, I work as a frontend developer at Simplify Technologies remotely where I work closely with the backend team to create services for microfinance organizations.</p>
         <p>I am skilled in HTML, CSS, JavaScript, and modern frontend frameworks like React, Next.js, component libraries like Material UI and state management libraries like RTK. I’m also familiar with designing restful APIs with Node.js and Content Management Systems(CMS) to create full stack applications.</p>
     </div>
   </div>
